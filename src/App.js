ccb0d415766f798dbeebd9e49fdecc01ba348d83
paulzay <paulotieno2@gmail.com>
2020-12-06 23:18:22 +0300

    build the rows, add styling and add packages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import './App.css';
+import Row from './Row';
+import requests from './requests';
+import Nav from './Nav';
+import Banner from './Banner';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Netflix clone app</h1>
-      </header>
+      <Nav />
+      <Banner />
+      <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals}/>
+      <Row title="Trending Now" isLargeRow fetchUrl={requests.fetchTrending}/>
+      <Row title="Top Rated" fetchUrl={requests.fetchTrending}/>
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}/>
     </div>
   );
 }
